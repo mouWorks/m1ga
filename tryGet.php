b7b10db8420a2608630f8e59c24477000ed9b867
MouDev-HACK <moudev-hack@Mou-ChundeMBP.mshome.net>
2017-07-29 01:17:27 +0800

    modify

--- tryGet.php
+++ tryGet.php
@@ -2,7 +2,6 @@
 $url = "http://www.104.com.tw/i/apis/jobsearch.cfm?cat=2001001001&area=6001001001&fmt=8&
 cols=J,JOB,NAME";
 $data = file_get_contents($url);
-//echo 'trying: use FileGetContents';
 $data = json_decode($data, TRUE);
 ///echo $data;
 
