5b96be9c495746b4be71e19134087198ee4ba5e3
MouDev-HACK <moudev-hack@Mou-ChundeMBP.mshome.net>
2017-07-29 00:12:36 +0800

    try another

--- tryGet.php
+++ tryGet.php
@@ -1,7 +1,12 @@
 <?php
-$url = "http://www.104.com.tw/i/apis/jobsearch.cfm?cat=2001001001&area=6001001001&fmt=4&
+$url = "http://www.104.com.tw/i/apis/jobsearch.cfm?cat=2001001001&area=6001001001&fmt=8&
 cols=J,JOB,NAME";
 $data = file_get_contents($url);
-echo 'trying: use FileGetContents';
+//echo 'trying: use FileGetContents';
+$data = json_decode($data, TRUE);
+
 var_dump($data);
+
+//echo json_encode($data);
+
 ?>
