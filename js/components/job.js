7ebbcdd9e6ff6d7b51f69c90408e792ebf7c99d6
TimChen <tim.chen@udngroup.com.tw>
2017-07-29 14:22:49 +0800

    bind joblist

--- js/components/job.js
+++ js/components/job.js
@@ -3,11 +3,33 @@ const jobView = {
 
   data () {
     return {
-      //
+      url: 'http://m1ga.win/php/q.php',
+      jobCount: null,
+      jobList: [],
+      message: ''
     }
   },
 
   methods: {
-    //
+    load (url) {
+      if (this.url.length > 0) {
+        this.$http.get(url).then(response => {
+          // get body data
+          console.log(response)
+          this.jobCount = Number(response.body.RECORDCOUNT)
+          this.jobList = response.body.data
+        }, response => {
+          // error callback
+          console.log(response)
+          this.message = response.status + ' ' + response.statusText
+        })
+      } else {
+        this.message = 'Please enter Url...'
+      }
+    }
+  },
+
+  mounted () {
+    this.load(this.url)
   }
 }
